0b767c738d4de3d4e5c0f17de885b65663e79eaa
ThiagoSallesSantos <thiagosallessantos3@gmail.com>
2021-08-28 23:27:16 -0300

    Criação da Classe C

---
+++ C.java
@@ -0,0 +1,15 @@
+public class C{
+	
+	//Atributos privados
+	private String C1;
+	private int C2;
+	
+	public void MC1(){
+		System.out.println("MC1()");
+	}
+
+	public void MC2(){
+		System.out.println("MC2()");
+	}
+
+}
