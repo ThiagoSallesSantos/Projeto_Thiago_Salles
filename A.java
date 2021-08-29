1c17ace2dee4cb0726021cb4545132b64891f209
ThiagoSallesSantos <thiagosallessantos3@gmail.com>
2021-08-29 11:30:30 -0300

    Criado o Método MA3()

--- A.java
+++ A.java
@@ -11,6 +11,10 @@ public class A{
 	public void MA2(){
 		System.out.println("MA2()");
 	}
+	
+	public void MA3(){
+		System.out.println("Alteração a classe A partir do clone");
+	}
 
 	public int getA1(){
 		return this.A1;
