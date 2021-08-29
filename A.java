cf0920ce6ef0354a3183d9dd7a8b6d04343d20ea
ThiagoSallesSantos <56984401+ThiagoSallesSantos@users.noreply.github.com>
2021-08-28 22:30:29 -0300

    Commit - Arquivos base
    Adicionando os arquivos contendo as classes A e B.

---
+++ A.java
@@ -0,0 +1,31 @@
+public class A{
+	
+	//Atributos privados
+	private int A1;
+	private float A2;
+	
+	public void MA1(){
+		System.out.println("MA1()");
+	}
+
+	public void MA2(){
+		System.out.println("MA2()");
+	}
+
+	public int getA1(){
+		return this.A1;
+	}
+	
+	public float getA2(){
+		return this.A2;
+	}
+	
+	public void setA1(int A1){
+		this.A1 = A1;
+	}
+	
+	public void setA2(int A2){
+		this.A2 = A2;
+	}
+
+}
---
+++ B.java
@@ -0,0 +1,31 @@
+public class B{
+	
+	//Atributos privados
+	private int B1;
+	private float B2;
+	
+	public void MB1(){
+		System.out.println("MB1()");
+	}
+	
+	public void MB2(){
+		System.out.println("MB2()");
+	}
+
+	public int getB1(){
+		return this.B1;
+	}
+
+	public float getB2(){
+		return this.B2;
+	}
+
+	public void setB1(int B1){
+		this.B1 = B1;
+	}
+
+	public void setB2(float B2){
+		this.B2 = B2;
+	}
+
+}
